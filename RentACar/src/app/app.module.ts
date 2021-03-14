2c410c4c60139f38c5d187b7371e9f39c487ba6b
mertaksu28 <mertaksu28@outlook.com.tr>
2021-03-14 07:37:15 +0300

    .

---
+++ RentACar/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { NaviComponent } from './components/navi/navi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarComponent,
+    BrandComponent,
+    NaviComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
